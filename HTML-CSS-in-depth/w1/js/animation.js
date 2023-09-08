33173a1dfa2114bc81227fd121964d861b0bf1e5
fakhirsh <fakhir.shaheen@gmail.com>
2023-09-08 23:21:18 +0500

    HTML and CSS course: HTML week 1

---
+++ HTML-CSS-in-depth/w1/js/animation.js
@@ -0,0 +1,32 @@
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+
+const speed = 4;
+let position = 0;
+let moveSpeed = speed;
+let radius = 10;
+
+function moveBall(){
+    if(position + radius > 640){
+        moveSpeed = -speed;
+    } else if(position - radius < 0){
+        moveSpeed = speed;
+    }
+    position += moveSpeed;
+}
+
+function drawBall(){
+    context.fillStyle = "#62687f";
+    context.beginPath();
+    context.arc(position, 50, radius, 0, Math.PI * 2, true);
+    context.fill();
+}
+
+function animate(){
+    
+    moveBall();
+    drawBall();
+    window.requestAnimationFrame(animate);
+}
+
+window.requestAnimationFrame(animate);
