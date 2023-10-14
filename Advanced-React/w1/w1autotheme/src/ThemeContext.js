5a885b286ea895d2feb7398692893a9025a22ea6
fakhirsh <fakhir.shaheen@gmail.com>
2023-10-14 14:03:36 +0500

    Week 1 Theme Lab

---
+++ Advanced-React/w1/w1autotheme/src/ThemeContext.js
@@ -0,0 +1,13 @@
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState("light");
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    };
+    return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+};
+
+export const useTheme = () => useContext(ThemeContext);
